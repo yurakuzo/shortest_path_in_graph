933061ec00f6d061aeaa24c78477b674176f213e
yurakuzo <yurakuzo20@gmail.com>
2021-08-12 17:41:15 +0300

    Few fixes

--- my_library.py
+++ my_library.py
@@ -1,4 +1,32 @@
+import shortestwaygraph as swg
 
+ds = swg.DijkstraSearch(9)
+fws = swg.Floyd_WarshallSearch(6)
 
-if __name__ == "__main__":
-    pass
+ds.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+            [4, 0, 8, 0, 0, 0, 0, 11, 0],
+            [0, 8, 0, 7, 0, 4, 0, 0, 2],
+            [0, 0, 7, 0, 9, 14, 0, 0, 0],
+            [0, 0, 0, 9, 0, 10, 0, 0, 0],
+            [0, 0, 4, 14, 10, 0, 2, 0, 0],
+            [0, 0, 0, 0, 0, 2, 0, 1, 6],
+            [8, 11, 0, 0, 0, 0, 1, 0, 7],
+            [0, 0, 2, 0, 0, 0, 6, 7, 0]]
+
+fws.graph = [[0, 1, 6, fws.INF, fws.INF, fws.INF],
+            [fws.INF, 0, 4, fws.INF, -2, fws.INF],
+            [fws.INF, fws.INF, 0, fws.INF, 5, 3],
+            [2, fws.INF, fws.INF, 0, -5, fws.INF],
+            [fws.INF, fws.INF, fws.INF, 8, 0, 4],
+            [fws.INF, fws.INF, fws.INF, fws.INF, fws.INF, 0]]
+
+ds.dijkstra(0, 8)
+print("-"*26)
+fws.floyd_warshall(fws.graph)
+
+# question:
+# def foo(a, *args):
+#       if not args:
+#             pass
+#       elif args:
+#             pass
--- shortestwaygraph.py
+++ shortestwaygraph.py
@@ -14,7 +14,9 @@ def visualize_graph(graph):
                 x = j
                 y = graph[:i + 1].index(graph[i])
                 visual.append([x, y, {'weight': graph[x][y]}])
-    numpy.random.seed(22222) #111, 666,666666, 22222, 2040021520
+    numpy.random.seed(2040021520)
+    # 111, 666,666666, 22222, 2040021520
+    # few seeds that also work
     G = nx.Graph()
     G.add_edges_from(visual)
     nx.draw_networkx(G)
@@ -26,30 +28,28 @@ class DijkstraSearch():
         self.graph = [[0 for column in range(vertices)] for row in range(vertices)]
 
 
-    def printSolution(self, dist, src, *args):
-        if not args:
-            for node in range(self.V):
-                print(f"Дистанція від {src} до {node} = {dist[node]}")
-        if args:
-            for node in range(args[0] + 1):
-                print(f"Дистанція від {src} до {node} = {dist[node]}")
-            # print(f"Дистанція від {src} до {args[0]} = {dist[args[0]]}")
-        visualize_graph(self.graph)
-
-
     def minDistance(self, dist, sptSet):
-        min = sys.maxsize
+        min = sys.maxsize # Initialize minimum distance for next node
         for v in range(self.V):
-            if dist[v] < min and sptSet[v] == False:
+            if dist[v] < min and not sptSet[v]:
                 min = dist[v]
                 min_index = v
         return min_index
 
+    def printSolution(self, dist, src, *args):
+        if not args:
+            for node in range(self.V):
+                print(f"Distance from {src} to {node} = {dist[node]}")
+        if args:
+            for node in range(args[0] + 1):
+                print(f"Distance from {src} to {node} = {dist[node]}")
+            # print(f"Дистанція від {src} до {args[0]} = {dist[args[0]]}")
+        visualize_graph(self.graph)
 
-    def dijkstra(self, src, *args):
+    def dijkstra(self, start, *args):
         if not args:
             dist = [sys.maxsize] * self.V
-            dist[src] = 0
+            dist[start] = 0
             sptSet = [False] * self.V
             for cout in range(self.V):
                 u = self.minDistance(dist, sptSet)
@@ -58,11 +58,11 @@ class DijkstraSearch():
                     if self.graph[u][v] > 0 and not sptSet[v] and\
                         dist[v] > dist[u] + self.graph[u][v]:
                         dist[v] = dist[u] + self.graph[u][v]
-            self.printSolution(dist, src)
+            self.printSolution(dist, start)
 
         if args:
             dist = [sys.maxsize] * self.V
-            dist[src] = 0
+            dist[start] = 0
             sptSet = [False] * self.V
             for cout in range(self.V):
                 u = self.minDistance(dist, sptSet)
@@ -71,26 +71,27 @@ class DijkstraSearch():
                     if self.graph[u][v] > 0 and sptSet[v] == False and\
                         dist[v] > dist[u] + self.graph[u][v]:
                         dist[v] = dist[u] + self.graph[u][v]
-            self.printSolution(dist, src, args[0])
+            self.printSolution(dist, start, args[0])
 
 
 class Floyd_WarshallSearch():
     def __init__(self, n):
         self.INF = 999
         self.V = n
-        self.graph =  [[0 for column in range(n)]
+        self.graph =  [[0 for col in range(n)]
                       for row in range(n)]
-        self.matrix = [[0 for col in range(self.V)] for row in range(self.V)]
+        self.matrix = [[0 for col in range(n)] for row in range(n)]
 
 
     def floyd_warshall(self, graph):
-
         distance = list(map(lambda i: list(map(lambda j: j, i)), graph))
         for k in range(self.V):
             for i in range(self.V):
                 for j in range(self.V):
                     distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
         self.print_solution(distance)
+        visualize_graph(self.matrix)
+
     def print_solution(self, distance):
         for i in range(self.V):
             for j in range(self.V):
@@ -101,4 +102,3 @@ class Floyd_WarshallSearch():
                     print(distance[i][j], end="  ")
                     self.matrix[i][j] = distance[i][j]
             print(" ")
-        visualize_graph(self.matrix)
